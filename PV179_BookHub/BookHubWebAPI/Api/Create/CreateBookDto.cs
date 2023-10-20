d0355975855cc071871c7e79bbdc823fc7ea2805
xmihale <540468@mail.muni.cz>
2023-10-20 16:41:49 +0200

    Fixing required and optional properties

--- PV179_BookHub/BookHubWebAPI/Api/Create/CreateBookDto.cs
+++ PV179_BookHub/BookHubWebAPI/Api/Create/CreateBookDto.cs
@@ -5,9 +5,9 @@ namespace BookHubWebAPI.Api.Create;
 public class CreateBookDto
 {
     public string Title { get; set; }
-    public string Author { get; set; }
-    public string Publisher { get; set; }
+    public string? Author { get; set; }
+    public string? Publisher { get; set; }
     public BookGenre BookGenre { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public double Price { get; set; }
 }
