29337926afe4a9e19f12c7cd388be864ab85d742
Makaveli1O <samuel.liska49@gmail.com>
2023-10-23 14:25:12 +0200

    feat: create UserController and setup user mapping

---
+++ PV179_BookHub/BookHubWebAPI/Mappers/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BookHubWebAPI.Api.Create;
+using BookHubWebAPI.Api.View;
+using DataAccessLayer.Models;
+
+namespace BookHubWebAPI.Mappers
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile() 
+        {
+            CreateMap<CreateUserDto, User>();
+            CreateMap<User,  GeneralUserViewDto>();
+        }
+    }
+}
