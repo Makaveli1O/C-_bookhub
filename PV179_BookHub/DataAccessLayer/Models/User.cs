0e71f3146ed3328ed16d0d18655b12a191a2411e
Makaveli1O <samuel.liska49@gmail.com>
2023-10-23 09:46:15 +0200

    feat: add User model

---
+++ PV179_BookHub/DataAccessLayer/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessLayer.Models
+{
+    public class User : BaseEntity
+    {
+        [MaxLength(100)]
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+        public string Salt { get; set; }
+        public UserRole Role { get; set; }
+        public virtual IEnumerable<BookReview>? BookReviews { get; set; }
+        public virtual IEnumerable<WishList>? WishLists { get; set; }
+        public virtual IEnumerable<Order>? Orders { get; set; }
+    }
+}
