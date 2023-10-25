517a5c11837714b9ad34a89ed85f4360d1152395
Makaveli1O <samuel.liska49@gmail.com>
2023-10-25 08:52:04 +0200

    feat: add Address entity

---
+++ PV179_BookHub/DataAccessLayer/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessLayer.Models;
+
+public class Address : BaseEntity
+{
+    [MaxLength(100)]
+    public string? Street;
+    [MaxLength(25)]
+    public string? StreetNumber;
+    [MaxLength(85)]
+    public required string City;
+    [MaxLength(20)]
+    public required string State;
+    [MaxLength(10)]
+    public required string PostalCode;
+}
