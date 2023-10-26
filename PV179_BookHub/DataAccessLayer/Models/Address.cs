a0b6982a8fed493282b79cbdbc73b6cfe0cec05a
Makaveli1O <samuel.liska49@gmail.com>
2023-10-26 07:48:41 +0200

    fix: edit address model property sizes

--- PV179_BookHub/DataAccessLayer/Models/Address.cs
+++ PV179_BookHub/DataAccessLayer/Models/Address.cs
@@ -4,13 +4,13 @@ namespace DataAccessLayer.Models;
 
 public class Address : BaseEntity
 {
-    [MaxLength(85)]
+    [MaxLength(30)]
     public required string City { get; set; }
-    [MaxLength(100)]
+    [MaxLength(30)]
     public string? Street { get; set; }
-    [MaxLength(25)]
+    [MaxLength(10)]
     public string? StreetNumber { get; set; }
-    [MaxLength(20)]
+    [MaxLength(10)]
     public required string State { get; set; }
     [MaxLength(10)]
     public required string PostalCode { get; set; }
