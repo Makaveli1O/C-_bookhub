1a90463822a3faf9425541c9cf735c42e8d2c3c6
Oliver Sintaj <oliver.sintaj@comap-control.com>
2023-10-29 10:10:33 +0100

    Added seeding

--- PV179_BookHub/DataAccessLayer/Models/WishList.cs
+++ PV179_BookHub/DataAccessLayer/Models/WishList.cs
@@ -6,9 +6,8 @@ namespace DataAccessLayer.Models;
 public class WishList : BaseEntity
 {
     public long UserId { get; set; }
-    /* @todo  Uncomment when User entity added */
-    //[ForeignKey(nameof(UserId))]
-    //public virtual User? Creator {  get; set; }
+    [ForeignKey(nameof(UserId))]
+    public virtual User? Creator { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     [MaxLength(500)]
     public string? Description {  get; set; }
