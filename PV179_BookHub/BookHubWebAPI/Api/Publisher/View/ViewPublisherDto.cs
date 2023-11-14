2b08507eabd3a942ce72367316a65fd409dd1721
Jozef Mihale <xmihale@fi.muni.cz>
2023-11-14 20:11:18 +0100

    Controller for Publishers

---
+++ PV179_BookHub/BookHubWebAPI/Api/Publisher/View/ViewPublisherDto.cs
@@ -0,0 +1,7 @@
+﻿namespace BookHubWebAPI.Api.Publisher.View;
+
+public class ViewPublisherDto
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+}
---
+++ PV179_BookHub/BookHubWebAPI/Mappers/PublisherProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BookHubWebAPI.Api.Publisher.Create;
+using BookHubWebAPI.Api.Publisher.View;
+using DataAccessLayer.Models.Publication;
+
+namespace BookHubWebAPI.Mappers;
+
+public class PublisherProfile : Profile
+{
+    public PublisherProfile()
+    {
+        CreateMap<CreatePublisherDto, Publisher>();
+        CreateMap<Publisher, ViewPublisherDto>();
+    }
+}
