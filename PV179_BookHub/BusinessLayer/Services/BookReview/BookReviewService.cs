f865c5dad14cd4337b534fac0047d1c497d96d15
Makaveli1O <samuel.liska49@gmail.com>
2023-11-25 12:17:43 +0100

    feat: add bookReview service

---
+++ PV179_BookHub/BusinessLayer/Services/BookReview/BookReviewService.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Exceptions;
+using Infrastructure.UnitOfWork;
+using BookReviewEntity = DataAccessLayer.Models.Account.BookReview;
+
+namespace BusinessLayer.Services.BookReview;
+
+public class BookReviewService : GenericService<BookReviewEntity, long>, IBookReviewService
+{
+    public BookReviewService(IUnitOfWork unitOfWork) : base(unitOfWork)
+    {
+    }
+    public async Task<IEnumerable<BookReviewEntity>> FindByBookIdAsync(long id)
+    {
+        var bookReviews = await Repository.GetAllAsync(
+                x => x.BookId == id
+            );
+
+        if ( bookReviews == null )
+        {
+            throw new NoSuchEntityException<long>(
+                typeof(BookReviewEntity),
+                id
+            );
+        }
+
+        return bookReviews;
+    }
+
+    public async Task<IEnumerable<BookReviewEntity>> FindByUserIdAsync(long userId)
+    {
+        var bookReviews = await Repository.GetAllAsync(
+                x=> x.ReviewerId == userId
+            );
+
+        if( bookReviews == null )
+        {
+            throw new NoSuchEntityException<long>(
+                typeof(BookReviewEntity),
+                userId
+            );
+        }
+
+        return bookReviews;
+    }
+}
---
+++ PV179_BookHub/BusinessLayer/Services/BookReview/IBookReviewService.cs
@@ -0,0 +1,8 @@
+﻿using BookReviewEntity = DataAccessLayer.Models.Account.BookReview;
+namespace BusinessLayer.Services.BookReview;
+
+public interface IBookReviewService : IGenericService<BookReviewEntity, long>
+{
+    Task<IEnumerable<BookReviewEntity>> FindByBookIdAsync(long id);
+    Task<IEnumerable<BookReviewEntity>> FindByUserIdAsync(long userId);
+}
