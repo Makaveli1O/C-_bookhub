b8cd66465fef90da3ad3a0eebe95967f10a7e237
unknown <540468@mail.muni.cz>
2023-12-29 22:27:48 +0100

    Creating Dependency Injection handler for BL

--- PV179_BookHub/BookHubWebAPI/Program.cs
+++ PV179_BookHub/BookHubWebAPI/Program.cs
@@ -1,30 +1,9 @@
 using BookHubWebAPI.Middleware;
 using BookHubWebAPI.Swagger;
-using BusinessLayer.Facades.Address;
-using BusinessLayer.Facades.Author;
-using BusinessLayer.Facades.Book;
-using BusinessLayer.Facades.Order;
-using BusinessLayer.Facades.BookReview;
-using BusinessLayer.Facades.Publisher;
-using BusinessLayer.Facades.WishList;
-using BusinessLayer.Facades.User;
-using BusinessLayer.Mappers;
-using BusinessLayer.Services;
-using BusinessLayer.Services.Author;
-using BusinessLayer.Services.Book;
-using BusinessLayer.Services.InventoryItem;
-using BusinessLayer.Services.Order;
-using BusinessLayer.Services.BookReview;
-using BusinessLayer.Services.Publisher;
-using DataAccessLayer.Models.Logistics;
-using DataAccessLayer.Models.Preferences;
-using DataAccessLayer.Models.Publication;
-using DataAccessLayer.Models.Account;
-using DataAccessLayer.Models.Purchasing;
 using Microsoft.OpenApi.Models;
-using BusinessLayer.Facades.BookStore;
 using DataAccessLayer.DependencyInjection;
 using Infrastructure.DependencyInjection;
+using BusinessLayer.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -37,50 +16,10 @@ builder.Services.RegisterDALDependencies(configuration);
 
 builder.Services.RegisterInfrastructureDependencies();
 
-builder.Services.AddScoped<IGenericService<Address, long>, GenericService<Address, long>>();
-builder.Services.AddScoped<IAddressFacade, AddressFacade>();
-
-builder.Services.AddScoped<IInventoryItemService, InventoryItemService>();
-builder.Services.AddScoped<IInventoryItemFacade, InventoryItemFacade>();
-
-builder.Services.AddScoped<IAuthorService, AuthorService>();
-builder.Services.AddScoped<IAuthorFacade, AuthorFacade>();
-
-builder.Services.AddScoped<IGenericService<Publisher, long>, PublisherService>();
-builder.Services.AddScoped<IPublisherFacade, PublisherFacade>();
-
-builder.Services.AddScoped<IBookService, BookService>();
-builder.Services.AddScoped<IBookFacade, BookFacade>();
-
-builder.Services.AddScoped<IGenericService<WishList, long>, GenericService<WishList, long>>();
-builder.Services.AddScoped<IGenericService<WishListItem, long>, GenericService<WishListItem, long>>();
-builder.Services.AddScoped<IWishListFacade, WishListFacade>();
-
-builder.Services.AddScoped<IGenericService<User, long>, GenericService<User, long>>();
-builder.Services.AddScoped<IUserFacade, UserFacade>();
-
-builder.Services.AddScoped<IGenericService<OrderItem, long>, GenericService<OrderItem, long>>();
-
-builder.Services.AddScoped<IOrderService, OrderService>();
-builder.Services.AddScoped<IOrderFacade, OrderFacade>();
-builder.Services.AddScoped<IBookReviewService, BookReviewService>();
-builder.Services.AddScoped<IBookReviewFacade, BookReviewFacade>();
-
-builder.Services.AddScoped<IGenericService<BookStore, long>, GenericService<BookStore, long>>();
-builder.Services.AddScoped<IBookStoreFacade, BookStoreFacade>();
-
-builder.Services.AddAutoMapper(typeof(AddressProfile));
-builder.Services.AddAutoMapper(typeof(BookProfile));
-builder.Services.AddAutoMapper(typeof(BookReviewProfile));
-builder.Services.AddAutoMapper(typeof(BookStoreProfile));
-builder.Services.AddAutoMapper(typeof(OrderProfile));
-builder.Services.AddAutoMapper(typeof(UserProfile));
-builder.Services.AddAutoMapper(typeof(WishListItemProfile));
-builder.Services.AddAutoMapper(typeof(WishListProfile));
-builder.Services.AddAutoMapper(typeof(AuthorBookAssociationProfile));
-
+builder.Services.RegisterBLDependencies();
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
