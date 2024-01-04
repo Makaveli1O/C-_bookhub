2f90b45a7a47184617e807a2bdd72e6ae6306a1e
Oliver Sintaj <oliver.sintaj@comap-control.com>
2024-01-04 20:56:17 +0100

    added request logging to mvc

--- PV179_BookHub/BookHubWebAPI/Middleware/RequestLoggingMiddleware.cs
+++ PV179_BookHub/BookHubWebAPI/Middleware/RequestLoggingMiddleware.cs
@@ -6,21 +6,24 @@ public class RequestLoggingMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly ILogger<RequestLoggingMiddleware> _logger;
+    private readonly string _sourceName;
 
-    public RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+    public RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger, string sourceName)
     {
         _next = next;
         _logger = logger;
+        _sourceName = sourceName;
     }
 
     public async Task Invoke(HttpContext context)
     {
-        _logger.LogInformation($"Received request: {context.Request.Method} {context.Request.Path}");
+        _logger.LogInformation($"{_sourceName} - Received request : {context.Request.Method} {context.Request.Path}");
 
         var watch = Stopwatch.StartNew();
         await _next(context);
         watch.Stop();
 
-        _logger.LogInformation($"Response status code: {context.Response.StatusCode} and processing time: {watch.ElapsedMilliseconds} ms");
+        _logger.LogInformation($"{_sourceName} - Response status code: {context.Response.StatusCode} and processing time: {watch.ElapsedMilliseconds} ms");
     }
+
 }
