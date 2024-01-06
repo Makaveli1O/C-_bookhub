30d00371c6fe02261c6d89ab899003498b6a7021
unknown <540468@mail.muni.cz>
2024-01-06 21:30:38 +0100

    Fixing issues with removed interface IBookService

--- PV179_BookHub/BusinessLayer.Tests/ServiceTests/BookServiceTests.cs
+++ PV179_BookHub/BusinessLayer.Tests/ServiceTests/BookServiceTests.cs
@@ -1,4 +1,4 @@
-﻿using BusinessLayer.Services.Book;
+﻿using BusinessLayer.Services;
 using DataAccessLayer.Models.Publication;
 using Infrastructure.Repository;
 using Infrastructure.UnitOfWork;
@@ -43,7 +43,7 @@ public class BookServiceTests
 
         using (var scope = serviceProvider.CreateScope())
         {
-            var bookService = scope.ServiceProvider.GetRequiredService<IBookService>();
+            var bookService = scope.ServiceProvider.GetRequiredService<IGenericService<Book, long>>();
 
             var result = await bookService.CreateAsync(book);
 
@@ -65,7 +65,7 @@ public class BookServiceTests
 
         using (var scope = serviceProvider.CreateScope())
         {
-            var bookService = scope.ServiceProvider.GetRequiredService<IBookService>();
+            var bookService = scope.ServiceProvider.GetRequiredService<IGenericService<Book, long>>();
 
             var result = await bookService.UpdateAsync(book);
 
@@ -89,7 +89,7 @@ public class BookServiceTests
 
         using (var scope = serviceProvider.CreateScope())
         {
-            var bookService = scope.ServiceProvider.GetRequiredService<IBookService>();
+            var bookService = scope.ServiceProvider.GetRequiredService<IGenericService<Book, long>>();
             var result = await bookService.FindByIdAsync(id);
             Assert.NotNull(result);
             Assert.Equal(book.Id, result.Id);
