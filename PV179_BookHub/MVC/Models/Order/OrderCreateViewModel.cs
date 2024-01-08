d28348a2b77a4d3debd7fc77572f28005ef1adc9
Makaveli1O <samuel.liska49@gmail.com>
2024-01-08 12:33:57 +0100

    feat: introduce models and views

---
+++ PV179_BookHub/MVC/Models/Order/OrderCreateViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MVC.Models.Order;
+
+public class OrderCreateViewModel
+{
+    public long BookId { get; set; }
+    public long BookStoreId { get; set; }
+    public uint Quantity { get; set; }
+}
