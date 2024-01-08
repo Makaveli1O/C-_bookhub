04c9b812836a41c7ba9bf50c9e1516c5c614e227
Makaveli1O <samuel.liska49@gmail.com>
2024-01-08 12:35:51 +0100

    fix: user controller usings

--- PV179_BookHub/MVC/Controllers/UserController.cs
+++ PV179_BookHub/MVC/Controllers/UserController.cs
@@ -1,10 +1,4 @@
-﻿using BusinessLayer.Facades.Book;
-using BusinessLayer.Facades.BookReview;
-using BusinessLayer.Facades.Order;
-using BusinessLayer.Facades.User;
-using BusinessLayer.Facades.WishList;
-using DataAccessLayer.Models.Account;
-using Microsoft.AspNetCore.Authorization;
+﻿using DataAccessLayer.Models.Account;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
@@ -17,16 +11,9 @@ public class UserController : Controller
 {
     private readonly UserManager<LocalIdentityUser> _userManager;
 
-    private readonly JsonSerializerOptions _jsonSerializerOptions;
-
     public UserController(UserManager<LocalIdentityUser> userManager)
     {
         _userManager = userManager;
-
-        _jsonSerializerOptions = new JsonSerializerOptions
-        {
-            WriteIndented = true,
-        };
     }
     
     public ActionResult Index()
