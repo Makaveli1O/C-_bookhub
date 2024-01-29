ff23b61a94936c9695853cb3eff97ee510907f9f
Makaveli1O <samuel.liska49@gmail.com>
2024-01-29 10:22:11 +0100

    feat: book / user review views

--- PV179_BookHub/MVC/Controllers/BookReviewController.cs
+++ PV179_BookHub/MVC/Controllers/BookReviewController.cs
@@ -1,8 +1,12 @@
-﻿using BusinessLayer.Facades.BookReview;
+﻿using BusinessLayer.Facades.Book;
+using BusinessLayer.Facades.BookReview;
+using BusinessLayer.Facades.User;
 using DataAccessLayer.Models.Account;
+using Mapster;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Json;
+using MVC.Models.BookReview;
 
 namespace MVC.Controllers;
 
@@ -10,23 +14,57 @@ namespace MVC.Controllers;
 public class BookReviewController : Controller
 {
     private readonly IBookReviewFacade _bookReviewFacade;
+    private readonly IBookFacade _bookFacade;
+    private readonly IUserFacade _userFacade;
     private readonly UserManager<User> _userManager;
-    private readonly JsonSerializerOptions _jsonSerializerOptions;
 
-    public BookReviewController(IBookReviewFacade bookReviewFacade, UserManager<User> userManager)
+    public BookReviewController(
+        IBookReviewFacade bookReviewFacade,
+        IBookFacade bookFacade,
+        IUserFacade userFacade,
+        UserManager<User> userManager
+        )
     {
         _bookReviewFacade = bookReviewFacade;
+        _bookFacade = bookFacade;
+        _userFacade = userFacade;
         _userManager = userManager;
-        _jsonSerializerOptions = new JsonSerializerOptions
-        {
-            WriteIndented = true,
-        };
     }
 
-    [HttpGet("User/{id:long}")]
-    //[AllowAnonymous]
-    public async Task<JsonResult> SingleUserBookReviews(long id)
+    [HttpGet("User/{userId:long}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> SingleUserBookReviews(long userId)
     {
-        return Json(await _bookReviewFacade.FindUserReviewsAsync(id));
+        var userReviews = await _bookReviewFacade.FindUserReviewsAsync(userId);
+
+        List<BookReviewViewModel> model = new List<BookReviewViewModel>();
+        foreach (var userReview in userReviews)
+        {
+            var modelElement = userReview.Adapt<BookReviewViewModel>();
+            modelElement.Title = (await _bookFacade.FindBookByIdAsync(userReview.BookId)).Title;
+            modelElement.ReviewerName = (await _userFacade.FetchUserAsync(userReview.ReviewerId)).UserName;
+            model.Add(modelElement);
+        }
+
+        return View("UserReviews", model);
+    }
+
+    [HttpGet("Book/{id:long}")]
+    public async Task<IActionResult> BookReviews(long id)
+    {
+        var bookReviews = await _bookReviewFacade.FindBookReviewsAsync(id);
+
+        List<BookReviewViewModel> model = new List<BookReviewViewModel>();
+        foreach (var bookReview in bookReviews)
+        {
+            var modelElement = bookReview.Adapt<BookReviewViewModel>();
+            modelElement.Title = (await _bookFacade.FindBookByIdAsync(bookReview.BookId)).Title;
+            modelElement.ReviewerName = (await _userFacade.FetchUserAsync(bookReview.ReviewerId)).UserName;
+            model.Add(modelElement);
+        }
+
+        
+
+        return View(model);
     }
 }
---
+++ PV179_BookHub/MVC/Models/BookReview/BookReviewViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DataAccessLayer.Models.Enums;
+
+namespace MVC.Models.BookReview;
+
+public class BookReviewViewModel
+{
+    public string Title { get; set; }
+    public string ReviewerName { get; set; }
+    public string Description { get; set; }
+    public Rating Rating { get; set; }
+}
